5149583bd3c9fd3f61c9196a02d355785581eb28
Arunendra Singh <singharunendra978@gmail.com>
2024-05-14 21:25:04 +0530

    Card design

--- beverages/src/components/CocktailsCart.jsx
+++ beverages/src/components/CocktailsCart.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
-function CocktailsCart() {
+function CocktailsCart({detail}) {
+
+  const {strDrinkThumb, strDrink, strCategory, strAlcoholic, strInstructions} = detail
+  console.log(detail)
   return (
-    <div className='cocktail border-2 border-red-700'>
-        <img src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" alt="" />
-        <div>
-            <h3>Card Title</h3>
-            <p>                                                                                                                         kkkkkkkkk   </p>
+    <div className='cocktail shadow-xl rounded-2xl'>
+        <img src={strDrinkThumb} alt="" className='p-1 rounded-2xl'/>
+        <div className='p-4 flex flex-col justify-between'>
+            <h1 className='font-bold text-3xl text-sky-600 font-mono'>{strDrink}</h1>
+            <span className='font-semibold text-gray-400 font-mono'>{strCategory}, {strAlcoholic}</span>
+            <p className='text-slate-600 font-mono line-clamp-3'>{strInstructions}</p>
+            <button className='mt-4 bg-sky-600 hover:bg-sky-700 text-white px-3 py-1 rounded-md hover:ring-2 ring-offset-1 hover:ring-slate-500	w-2/6'>Details</button>
         </div>
     </div>
   )
--- beverages/src/components/Home.jsx
+++ beverages/src/components/Home.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Search from './Search'
 import CocktailsCart from './CocktailsCart'
 
 function Home() {
+  // Get the list of beverages using API, then present the list,
+  const [beverages, setBeverages] = useState([])
+
+  // Write a use effect to load the cocktails from server
+
+  useEffect(() => {
+    async function loadCocktails(){
+        const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=c");
+        const cocktails_list = await response.json();
+        let drinks = cocktails_list["drinks"]
+        // set the drinks
+        setBeverages(drinks)
+    }
+
+    loadCocktails()
+  }, [])
+
   return (
     <div><Search />
     <section className="section border">
       <h1 className='text-center text-4xl font-mono font-semibold hover:text-sky-600 hover:font-bold'>Cocktails</h1>
       <div className="cocktails-center w-11/12 mx-auto grid grid-cols-1 ss:grid-cols-2 ms:grid-cols-3 gap-3">
-         {Array.from({length: 30}, (v, i)=> <CocktailsCart key={i} />)}
+         {/* {Array.from({length: 30}, (v, i)=> <CocktailsCart key={i} />)} */}
+         {beverages.map((be, i) => <CocktailsCart key={i} detail={be} />)}
       </div>
     </section>
     </div>
